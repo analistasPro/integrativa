68f04a09c5c12259b1ec04a37bbfd60630d1e07a
Sebaplay <Rails>
2015-11-23 11:54:34 -0300

    asd
    qwe

--- db/schema.rb
+++ db/schema.rb
@@ -28,21 +28,9 @@ ActiveRecord::Schema.define(version: 20151123142304) do
     t.string   "section"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
   end
 
   add_index "books", ["category_id"], name: "index_books_on_category_id"
-  add_index "books", ["email"], name: "index_books_on_email", unique: true
-  add_index "books", ["reset_password_token"], name: "index_books_on_reset_password_token", unique: true
 
   create_table "categories", force: true do |t|
     t.string   "code"
